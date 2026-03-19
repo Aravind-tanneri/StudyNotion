b03794b9a1121a2e0c659b700d8ef52a65cfc8fd
Aravind Tanneri <tanneriaravind2007@gmail.com>
2026-03-19 14:34:21 +0530

    devops

--- frontend/src/services/operations/SettingsAPI.js
+++ frontend/src/services/operations/SettingsAPI.js
@@ -1,6 +1,6 @@
 import { toast } from "react-hot-toast"
 import { setUser } from "../../slices/profileSlice"
-import { apiConnector } from "../apiConnector"
+import { apiConnector } from "../apiconnector"
 import { settingsEndpoints } from "../apis"
 import { logout } from "./authAPI"
 
